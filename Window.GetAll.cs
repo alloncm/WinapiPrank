4b9c553d241299b67cdc602cdc960370de740791
YarinOmesi <56448176+YarinOmesi@users.noreply.github.com>
2024-07-20 16:05:26 +0300

    Feature/window (#1)
    Added `Window.cs` with the ability to 
    - Get visible all windows
    - Get Window Runnning Process
    - Get Window Rectable
    - Move Window By Rectangle
    - Get Window Title
    - Check if the window is in foreground
    - Minimize window

---
+++ Window.GetAll.cs
@@ -0,0 +1,46 @@
+﻿using System.Runtime.InteropServices;
+using static Windows.Win32.PInvoke;
+
+namespace WinapiPrank;
+
+internal partial class Window
+{
+    private record Parameters(List<Window> Windows, Filter Filter);
+
+    public static bool GetAll(Filter filter, out List<Window> windowsInfo)
+    {
+        windowsInfo = [];
+
+        Parameters parameters = new Parameters(windowsInfo, filter);
+        GCHandle gcHandle = GCHandle.Alloc(parameters, GCHandleType.Normal);
+
+        try
+        {
+            bool result = EnumWindows(static (handle, lParam) =>
+            {
+                Parameters parameters = (GCHandle.FromIntPtr(lParam).Target as Parameters)!;
+
+                bool visible = IsWindowVisible(handle);
+
+                if (parameters.Filter.HasFlag(Filter.Visible) && !visible) return true;
+
+                parameters.Windows.Add(new Window(handle, visible));
+
+                return true;
+            }, GCHandle.ToIntPtr(gcHandle));
+
+            return result;
+        }
+        finally
+        {
+            if (gcHandle.IsAllocated) gcHandle.Free();
+        }
+    }
+
+    [Flags]
+    public enum Filter
+    {
+        None = 0,
+        Visible = 1,
+    }
+}
---
+++ Window.cs
@@ -0,0 +1,68 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+using Windows.Win32.Foundation;
+using Windows.Win32.UI.WindowsAndMessaging;
+using static Windows.Win32.PInvoke;
+
+namespace WinapiPrank;
+
+internal partial class Window
+{
+    private readonly Lazy<string> _name;
+    private readonly Lazy<Process?> _process;
+
+    public HWND Handle { get; }
+    public bool IsVisible { get; }
+
+    public string Name => _name.Value;
+    public Process? Process => _process.Value;
+
+    private Window(HWND handle, bool isVisible)
+    {
+        Handle = handle;
+        IsVisible = isVisible;
+        _name = new Lazy<string>(() => GetWindowTitle(Handle));
+        _process = new Lazy<Process?>(() => GetProcess(Handle));
+    }
+
+    public bool Minimize()
+    {
+        WINDOWPLACEMENT windowplacement = default;
+        bool result = GetWindowPlacement(Handle, ref windowplacement);
+        if (result == false) return false;
+        windowplacement.showCmd = SHOW_WINDOW_CMD.SW_MINIMIZE;
+        return SetWindowPlacement(Handle, in windowplacement);
+    }
+
+    public bool IsInForeground() => GetForegroundWindow() == Handle;
+
+    public bool GetRect(out RECT rect)
+    {
+        return GetWindowRect(Handle, out rect);
+    }
+
+    public bool Move(in RECT rect)
+    {
+        return SetWindowPos(Handle, HWND.Null, rect.X, rect.Y, rect.Width, rect.Width,
+            SET_WINDOW_POS_FLAGS.SWP_NOZORDER | SET_WINDOW_POS_FLAGS.SWP_NOSIZE);
+    }
+
+    private static unsafe Process? GetProcess(HWND handle)
+    {
+        uint processId;
+        uint result = GetWindowThreadProcessId(handle, &processId);
+        return result == 0 ? null : Process.GetProcessById((int) processId);
+    }
+
+    private static unsafe string GetWindowTitle(HWND handle)
+    {
+        int length = GetWindowTextLength(handle);
+
+        if (length == 0) return string.Empty;
+
+        return string.Create(length + 1, (handle, length: length + 1), static (span, pair) =>
+        {
+            fixed (char* c = span) GetWindowText(pair.handle, c, pair.length);
+        });
+    }
+}
