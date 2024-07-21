ec36ecb0ece3573d606896b18db4037a48fed4e9
Yarin Omesi <yarinomesi@gmail.com>
2024-07-21 21:25:23 +0300

    Add Mouse.GetPos()

---
+++ Mouse.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+using static Windows.Win32.PInvoke;
+
+namespace WinapiPrank;
+
+public static class Mouse
+{
+    internal static bool GetPos(out Point point)
+    {
+        return GetCursorPos(out point);
+    }
+}
