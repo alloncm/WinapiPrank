f0b5f714a14d7f0a1889a024028bc5f34fd2ded5
Yarin Omesi <yarinomesi@gmail.com>
2024-07-22 22:02:52 -0700

    Add bsod

---
+++ BlueScreenOfDeath.cs
@@ -0,0 +1,46 @@
+﻿using System.Runtime.InteropServices;
+using Windows.Win32.Foundation;
+
+namespace WinapiPrank;
+
+public static class BlueScreenOfDeath
+{
+    private const uint STATUS_ASSERTION_FAILURE = 0xC0000420;
+
+    public static bool Trigger()
+    {
+#if EnabbleBSOD
+        RtlAdjustPrivilege(19, true, false, out bool previousValue);
+
+        NTSTATUS ntstatus = NtRaiseHardError(STATUS_ASSERTION_FAILURE,
+            0,
+            0,
+            IntPtr.Zero,
+            6,
+            out uint oul
+        );
+
+        return ntstatus == 0;
+#else
+        return false;
+#endif
+    }
+
+    [DllImport("ntdll.dll")]
+    private static extern uint RtlAdjustPrivilege(
+        int privilege,
+        bool bEnablePrivilege,
+        bool isThreadPrivilege,
+        out bool previousValue
+    );
+
+    [DllImport("ntdll.dll")]
+    private static extern NTSTATUS NtRaiseHardError(
+        uint errorStatus,
+        uint numberOfParameters,
+        uint unicodeStringParameterMask,
+        IntPtr parameters,
+        uint validResponseOption,
+        out uint response
+    );
+}
