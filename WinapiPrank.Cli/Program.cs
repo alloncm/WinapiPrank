cd96776b099f5d7d18328c1c64c5e6d30d8f8e38
Yarin Omesi <yarinomesi@gmail.com>
2024-07-26 15:17:05 +0300

    Add code to trigger falling windows from cli

--- WinapiPrank.Cli/Program.cs
+++ WinapiPrank.Cli/Program.cs
@@ -1,3 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Cocona;
+using WinapiPrank;
 
-Console.WriteLine("Hello, World!");
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CoconaApp.Run<Program>(args);
+    }
+
+    [Command(nameof(FallingWindows))]
+    public void FallingWindows([Option(['i'])] TimeSpan? interval)
+    {
+        var options = new FallingWindowsPistun.Options();
+
+        if (interval.HasValue) options.IntervalTime = interval.Value;
+
+        FallingWindowsPistun pistun = new FallingWindowsPistun(options);
+
+        pistun.Run();
+    }
+}
