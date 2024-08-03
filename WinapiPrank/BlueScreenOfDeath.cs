43a54f4d83a68b4b3cca07f214844ba9697bb013
YarinOmesi <56448176+YarinOmesi@users.noreply.github.com>
2024-08-03 17:32:49 +0300

    Feature/bsod (#4)
    Add class `BlueScreenOfDeath` that enbale you to trigger the bsod

---
+++ WinapiPrank/BlueScreenOfDeath.cs
@@ -0,0 +1,47 @@
+﻿using System.Runtime.InteropServices;
+using Windows.Win32.Foundation;
+
+namespace WinapiPrank;
+
+public static class BlueScreenOfDeath
+{
+    private const uint STATUS_ASSERTION_FAILURE = 0xC0000420;
+
+    public static void Trigger()
+    {
+#if EnabbleBSOD
+        RtlAdjustPrivilege(19, true, false, out bool previousValue);
+
+        NTSTATUS ntstatus = NtRaiseHardError(STATUS_ASSERTION_FAILURE,
+            0,
+            0,
+            IntPtr.Zero,
+            6,
+            out uint oul
+        );
+
+        if (ntstatus != 0)
+        {
+            throw new Exception($"Triggering BSOD failed with status {(int)ntstatus}");
+        }
+#endif
+    }
+
+    [DllImport("ntdll.dll")]
+    private static extern uint RtlAdjustPrivilege(
+        int privilege,
+        bool bEnablePrivilege,
+        bool isThreadPrivilege,
+        out bool previousValue
+    );
+
+    [DllImport("ntdll.dll")]
+    private static extern NTSTATUS NtRaiseHardError(
+        uint errorStatus,
+        uint numberOfParameters,
+        uint unicodeStringParameterMask,
+        IntPtr parameters,
+        uint validResponseOption,
+        out uint response
+    );
+}
