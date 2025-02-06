2ec7d57e2458f619ebe97d0dd0415a5376faca3f
alloncm <alloncm@gmail.com>
2025-02-07 00:11:25 +0200

    Shrink output exe size
    - Removed Cocona as a dependency (did not support AOT and trimming since it uses reflection)
    - Replaced PublishSingleFile with PublishAot (reduces output size)
    - Trim the output executable to reduce output size
    - Fix version and bump it

--- src/Program.cs
+++ src/Program.cs
@@ -1,32 +1,57 @@
-﻿using Cocona;
-using Windows.Win32.UI.Input.KeyboardAndMouse;
+﻿using Windows.Win32.UI.Input.KeyboardAndMouse;
 using Windows.Win32.UI.WindowsAndMessaging;
 using Windows.Win32.Foundation;
 using static Windows.Win32.PInvoke;
 
 namespace WinapiPrank;
 
-public class Program : CoconaConsoleAppBase
+public class Program
 {
-    public static void Main(string[] args)
+    public static int Main(string[] args)
     {
-        CoconaApp.Run<Program>(args);
+        // In C# the program name is not supplied in the command line args
+        if (args.Length < 1)
+        {
+            Console.WriteLine($"Not enough arguemnts supplied, found {args.Length}");
+            return 1;
+        }
+        CancellationTokenSource tokenSource = new();
+        Console.CancelKeyPress += (_, e) => 
+        {
+            e.Cancel = true;    // Wait for the program to terminate gracefuly 
+            tokenSource.Cancel();
+        };
+
+        return args[0] switch
+        {
+            "key-hook"=>KeyHook(tokenSource.Token),
+            "falling-windows"=>FallingWindows(tokenSource.Token, args.ElementAtOrDefault(1)),
+            _=>1
+        };
     }
 
-    [Command(nameof(FallingWindows))]
-    public void FallingWindows([Option(['i'])] TimeSpan? interval)
+    public static int FallingWindows(CancellationToken token, string? inputInterval)
     {
         var options = new FallingWindowsPistun.Options();
-
-        if (interval.HasValue) options.IntervalTime = interval.Value;
+        if (inputInterval is not null)
+        {
+            if (TimeSpan.TryParse(inputInterval, out var interval ))
+            {
+                options.IntervalTime = interval;
+            }
+            else
+            {
+                return 1;
+            }
+        }
 
         FallingWindowsPistun pistun = new FallingWindowsPistun(options);
 
-        pistun.Run(Context.CancellationToken);
+        pistun.Run(token);
+        return 0;
     }
 
-    [Command(nameof(KeyHook))]
-    public int KeyHook()
+    public static int KeyHook(CancellationToken token)
     {
 #if EnableBSOD
         var action = BlueScreenOfDeath.Trigger;
@@ -37,7 +62,7 @@ public class Program : CoconaConsoleAppBase
         {
             using var keyHook = new KeyHook(VIRTUAL_KEY.VK_LCONTROL, TimeSpan.FromMilliseconds(300), 3, action);
             Console.WriteLine("Hook installed successfully");
-            keyHook.Run(Context.CancellationToken);
+            keyHook.Run(token);
         }
         catch (Exception ex)
         {
